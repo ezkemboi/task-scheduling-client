402f58efe933ff691917249259fe022ededccbe1
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-17 13:34:56 +0300

    get tasks and display in table

--- src/pages/tasks.jsx
+++ src/pages/tasks.jsx
@@ -1,59 +1,76 @@
 import React from 'react';
+import axios from 'axios';
 
-function Tasks() {
+class Tasks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    }
+  }
 
-  return (
-    <div className="Tasks">
-      <div className="Tasks__items">
-        <table border="1">
-          <tr>
-            <th>Task Id</th>
-            <th>Customer First Name</th>
-            <th>Personal First Name</th>
-            <th>Personal Other Name</th>
-            <th>Customer Last Name</th>
-            <th>Customer Phone Number</th>
-          </tr>
-          <tr>
-            <td>20190</td>
-            <td>John</td>
-            <td>Legend</td>
-            <td>Philip</td>
-            <td>Paul</td>
-            <td>0789345687</td>
-            <td className="more-details">All Details</td>
-          </tr>
-          <tr>
-            <td>14672</td>
-            <td>King</td>
-            <td>John</td>
-            <td>Paul</td>
-            <td>James</td>
-            <td>07988245687</td>
-            <td className="more-details">All Details</td>
-          </tr>
-          <tr>
-            <td>20190</td>
-            <td>John</td>
-            <td>Legend</td>
-            <td>Philip</td>
-            <td>Paul</td>
-            <td>0789345687</td>
-            <td className="more-details">All Details</td>
-          </tr>
-          <tr>
-            <td>14672</td>
-            <td>King</td>
-            <td>John</td>
-            <td>Paul</td>
-            <td>James</td>
-            <td>07988245687</td>
-            <td className="more-details">All Details</td>
-          </tr>
-        </table>
+  componentDidMount() {
+    axios.get("http://localhost:8000/tasks/assigned").then(res => {
+      const { tasks } = res.data;
+      this.setState({
+        tasks
+      })
+    }).catch(err => {
+      alert(`An error occurred ${err}`)
+    })
+  }
+
+  render() {
+    const { tasks } = this.state;
+
+    let tasksItems;
+
+    tasksItems = tasks.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item.id}</td>
+          <td>{item.customerFirstName} {item.customerLastName}</td>
+          <td>{item.personnelFirstName} {item.personnelLastName}</td>
+          <td>{item.customePhoneNo}</td>
+          <td>{item.agentId}</td>
+          <td>{item.status}</td>
+          <td>{item.location}</td>
+          <td>{item.gender}</td>
+          <td>{item.age}</td>
+          <td>{item.comments}</td>
+          <td>{item.registration}</td>
+        </tr>
+      )
+    })
+
+    return (
+      <div className="Tasks">
+        <div className="Tasks__items">
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Task Id</th>
+                <th>Customer Name</th>
+                <th>Personal Name</th>
+                <th>Customer Phone Number</th>
+                <th>Agent Id</th>
+                <th>Task Status</th>
+                <th>Location</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Comments</th>
+                <th>Registration</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasksItems}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
 
 export default Tasks;
